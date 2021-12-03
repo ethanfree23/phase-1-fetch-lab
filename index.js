66f378f69246814f5ef86fab9dfb1f3ef5603a78
Ethan Freeman <ethanfree23@gmail.com>
2021-12-02 20:33:12 -0600

    Completed assingment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function fetchBooks() {
+function fetchBooks(){
+  fetch("https://anapioficeandfire.com/api/books")
   // To pass the tests, don't forget to return your fetch!
-  
+  .then(resp => resp.json())
+  .then(books => renderBooks(books))
+  return fetch("https://anapioficeandfire.com/api/books");
 }
 
 function renderBooks(books) {
@@ -15,3 +18,24 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+// function fetchBooks() {
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(res => res.json())
+//   .then(book =>renderBooks(book))
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
